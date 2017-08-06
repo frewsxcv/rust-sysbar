1b33cec200f04c0f0f347faa2eb35c4efbc8e63a
Corey Farwell <coreyf@rwell.org>
2017-08-06 17:20:34 -0400

    Simplify example.

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,46 +1,12 @@
-#![allow(non_snake_case)]
-
-use std::collections::HashMap;
-use std::sync::{Arc, RwLock};
-use std::mem;
-
 extern crate sysbar;
-use sysbar::Sysbar;
 
 fn main() {
-    let mut bar = Sysbar::new("Foo");
+    let mut bar = sysbar::Sysbar::new("Foo");
 
-    // make a hash map for the callbacks to mess with
-    let hm: HashMap<String, String> = HashMap::new();
-    let hm = Arc::new(RwLock::new(hm));
-
-    let phm = hm.clone();
     bar.add_item(
-        "Prefs",
+        "Say 'bar'",
         Box::new(move || {
-            let mut hm = phm.write().unwrap();
-            let size = hm.len();
-            let k = format!("Prefs{}", size);
-            hm.insert(k, "Bar".to_owned());
-
-            println!("prefs selected, new hm {:?}", *hm);
-        }),
-    );
-
-    let fhm = hm.clone();
-    bar.add_item(
-        "Summon Herb",
-        Box::new(move || {
-            let mut hm = fhm.write().unwrap();
-            let size = hm.len();
-            let k = format!("Herb{}", size);
-            hm.insert(k, "Bar".to_owned());
-
-            println!(
-                "Herb thinks you are a jerk and refuses to appear.  By the way, \
-                                the hash map is: {:?}",
-                *hm
-            );
+            println!("bar");
         }),
     );
 
