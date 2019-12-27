dc85c6a2a2af80e44026f71101a32289a95b70dd
Corey Farwell <coreyf@rwell.org>
2019-12-27 09:14:56 -0800

    remove println debugging

--- src/mac_os/mod.rs
+++ src/mac_os/mod.rs
@@ -127,7 +127,6 @@ impl Callback {
 
         let ptr = Box::into_raw(bcbs);
         let ptr = ptr as *mut c_void as usize;
-        println!("{}", ptr);
         let mut oid = <Callback as INSObject>::new();
         (*oid).setptr(ptr);
         oid
@@ -136,7 +135,6 @@ impl Callback {
     fn setptr(&mut self, uptr: usize) {
         unsafe {
             let obj = &mut *(self as *mut _ as *mut ::objc::runtime::Object);
-            println!("setting the ptr: {}", uptr);
             obj.set_ivar("_cbptr", uptr);
         }
     }
@@ -151,20 +149,17 @@ impl INSObject for Callback {
 
         let mut klass = Class::get(cname);
         if klass.is_none() {
-            println!("registering class for {}", cname);
             let superclass = NSObject::class();
             let mut decl = ClassDecl::new(&cname, superclass).unwrap();
             decl.add_ivar::<usize>("_cbptr");
 
             extern "C" fn sysbar_callback_call(this: &Object, _cmd: Sel) {
-                println!("callback, getting the pointer");
                 unsafe {
                     let pval: usize = *this.get_ivar("_cbptr");
                     let ptr = pval as *mut c_void;
                     let ptr = ptr as *mut CallbackState;
                     let bcbs: Box<CallbackState> = Box::from_raw(ptr);
                     {
-                        println!("cb test from cb");
                         (*bcbs.cb)();
                     }
                     mem::forget(bcbs);
